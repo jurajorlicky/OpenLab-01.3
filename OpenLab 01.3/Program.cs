cbf400a3ca8a9bd13b76bd04b59e703e52ec3f50
z18ista21 <z18ista21@Z00P1-03.SPSKNM>
2019-11-04 09:13:04 +0100

    Add project files.

---
+++ OpenLab 01.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OpenLab_01._3
+{
+    class Program
+    {
+        public static int (int input)
+            {
+            int sucet+1 = (input + 1);
+            return sucet;
+            }
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Zadaj číslo");
+        int input = int.Parse(Console.ReadLine());
+        int sucet+1 = (input);
+        Console.WriteLine($"Výsledne číslo je {sucet+1}");
+        Console.ReadLine();
+        }
+    }
+}
